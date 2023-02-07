7607f72f49cbe47f6042b4aeb9ee05795f0d85dd
Achmad Faiz Alawi <achmad.alawi@gmail.com>
2023-02-07 16:55:38 +0700

    Pengumpulan Tugas 3

---
+++ Tugas-3-Looping/looping.dart
@@ -0,0 +1,59 @@
+/*
+//Task Number 1
+void main(){
+  int number = 2;
+  print("LOOPING PERTAMA");
+  do{
+    if(number%2 == 0){
+      print("$number - I love coding");
+    }
+    number++;
+  } while(number <= 20);
+
+  print("LOOPING KEDUA");
+  do{
+    if(number%2 == 0){
+      print("$number - I will become a mobile developer");
+    }
+    number--;
+  } while(number > 0);
+}
+*/
+
+/*
+//Task Number 2
+void main(){
+  for(int i = 1; i <= 20; i++){
+    if(i%2 != 0 && i%3 == 0){
+      print("$i - I Love Coding");
+    } else if(i%2 != 0){
+      print("$i - Santai");
+    } else if(i%2 == 0){
+      print("$i - Berkualitas");
+    }
+  }
+}
+*/
+
+/*
+//Task Number 3
+void main(){
+  int length = 8;
+  int width = 4;
+
+  for(int i = 1; i <= width; i++){
+    print("#"*length);
+  }
+}
+*/
+
+/*
+//Task Number 4
+void main(){
+  int height = 7;
+
+  for(int i = 1; i <= height; i++){
+    print("#"*i);
+  }
+}
+*/
