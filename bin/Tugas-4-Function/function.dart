dac343a44e39a960cc3d2cf98efe72480c26d8bf
Achmad Faiz Alawi <achmad.alawi@gmail.com>
2023-02-09 18:08:46 +0700

    Pengumpulan Tugas 4

---
+++ bin/Tugas-4-Function/function.dart
@@ -0,0 +1,61 @@
+/*
+//Number 1
+void main(){
+  print(teriak());
+}
+
+String teriak(){
+  return "Halo Sanbers!";
+}
+*/
+
+/*
+//Number 2
+void main(){
+  final double num1 = 12;
+  final double num2 = 4;
+
+  final double hasilKali = kalikan(num1, num2);
+  print(hasilKali.toInt());
+}
+
+double kalikan(final double number1, final double number2){
+  return number1*number2;
+}
+*/
+
+/*
+//Number 3
+void main(){
+  final String name = "Aal";
+  final int age = 21;
+  final String alamat = "Bandung";
+  final String hobi = "Gaming";
+
+  final perkenalan = introduce(name, age, alamat, hobi);
+  print(perkenalan);
+}
+
+introduce(final String nama, final int umur, final String address, final String hobby){
+  return "Nama saya $nama, umur saya $umur tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby!";
+}
+*/
+
+/*
+//Number 4
+void main(){
+  print(faktorial(6));
+}
+
+double faktorial(final double angka){
+  double hasilFaktorial = 1;
+  if(angka <= 0){
+    return 1;
+  } else{
+    for(double kelipatan = 1; kelipatan <= angka; kelipatan++){
+      hasilFaktorial *= kelipatan;
+    }
+    return hasilFaktorial;
+  }
+}
+*/
